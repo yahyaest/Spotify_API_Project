829e379d8bea6ff1f4e911fa2eda6460cd9a1249
yahyaest <53539729+yahyaest@users.noreply.github.com>
2020-03-31 19:52:12 +0200

    Update oauth.py
    Security change : removing personel token

--- oauth.py
+++ oauth.py
@@ -1,12 +1,12 @@
-client_id  = '158cbf4e7b85401e9ea4c1c43ac84d32'
-client_secret  = 'cc164a86271c49beba12f94dbd8679b0'
+client_id  = '*****'
+client_secret  = '****'
 redirect_uri  = 'https://www.google.com/'
 scope = 'playlist-read-private'
 #List of Diffrent Scope:
 
 
 #twilio_token
-account_sid = 'AC691791f455a420e34da6aca49dc5777a'
-auth_token = '76f9495703bf7090388f97f6254501ac'
-my_cell = '+21658668989'
-my_twilio = '+19802951159'
+account_sid = '****'
+auth_token = '****'
+my_cell = '+***'
+my_twilio = '+****'
